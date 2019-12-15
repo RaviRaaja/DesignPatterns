0dd6a4744e88c02ad57333d01f83d4da88be7986
Rv <mailstoraviraaja@gmail.com>
2019-12-15 09:47:40 +0530

    update for observer pattern

--- ObserverPattern/src/Main.java
+++ ObserverPattern/src/Main.java
@@ -1,11 +1,13 @@
 public class Main { // Weather station - Main class
-    // Here we assume that values entered manually are
-    // obtained from the sensors located in the weather station
-    // Weather station will communicate using the same SINGLE object
-    // which we call as weather object.
-    // In observer pattern, object from the concrete implementation is loosely coupled
-    // we can see that both concrete implementation dealing with other classes object
-    // without inheriting / aggregation.
+     /**
+      * Here we assume that values entered manually are
+      * obtained from the sensors located in the weather station
+      * Weather station will communicate using the same SINGLE object
+      * which we call as weather object.
+      * In observer pattern, object from the concrete implementation is loosely coupled
+      * we can see that both concrete implementation dealing with other classes object
+      * without inheriting / associations.
+      */
 
     public static void main(String[] args) {
         // lets create a weather object
@@ -13,13 +15,19 @@ public class Main { // Weather station - Main class
 
         // lets create some display classes and pass weatherObject into it, so that those display know what is data to display.
         // below lines will indirectly register themselves into the observable / Subject - check its constructor
-        // first er should register by creating objects for the observer classes
+
         PhoneDisplay pDisplay = new PhoneDisplay(weatherObject);
         ComputerDisplay cDisplay = new ComputerDisplay(weatherObject);
 
         weatherObject.setMeasurements(67, 5.7);
         weatherObject.setMeasurements(123, 51.7);
         weatherObject.setMeasurements(23, 1.7);
+
+        weatherObject.unregister(pDisplay);
+        weatherObject.setMeasurements(11, 5.7);
+        weatherObject.setMeasurements(213, 111.7);
+
+
 
 
     }
